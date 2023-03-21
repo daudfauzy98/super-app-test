25a7797d3e9a1bb760eeac6345f80d3762ef665c
daudfauzy98 <daudfauzy@gmail.com>
2023-03-21 14:05:17 +0700

    Add payload validation

--- entity/product.go
+++ entity/product.go
@@ -2,8 +2,8 @@ package entity
 
 type Product struct {
 	ID       string  `gorm:"size:36;not null;primaryKey" json:"id"`
-	Name     string  `gorm:"size:100" json:"name"`
-	Category string  `gorm:"size:50" json:"category"`
-	Brand    string  `gorm:"size:50" json:"brand"`
-	Stocks   []Stock `json:"stocks"`
+	Name     string  `gorm:"size:100;not null" json:"name" binding:"required"`
+	Category string  `gorm:"size:50;not null" json:"category" binding:"required"`
+	Brand    string  `gorm:"size:50; not null" json:"brand" binding:"required"`
+	Stocks   []Stock `json:"stocks" binding:"required"`
 }
--- entity/stock.go
+++ entity/stock.go
@@ -4,6 +4,6 @@ type Stock struct {
 	ID           string `gorm:"size:36;not null;uniqueIndex;primaryKey" json:"id"`
 	ProductID    string `gorm:"size:36;not null" json:"product_id"`
 	WarehouseID  string `gorm:"size:36;not null" json:"warehouse_id"`
-	WarehouseLoc string `gorm:"size:100" json:"warehouse_loc"`
-	Count        int    `gorm:"size:10" json:"count"`
+	WarehouseLoc string `gorm:"size:100;not null" json:"warehouse_loc" binding:"required"`
+	Count        int    `gorm:"size:10;not null" json:"count" binding:"required"`
 }
